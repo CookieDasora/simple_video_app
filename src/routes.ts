4a8fafcb7d48cdfe0396fc1883a244ae6498ffef
CookieDasora <valterliviaamaral@gmail.com>
2022-01-03 21:34:56 -0300

    Testing...

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,4 @@ router.post('/users/create', new createUserController().handle);
 router.get('/users', new getAllUsersController().handle);
 router.get('/users/:id', new getUserController().handle);
 
-router.put('/');
-
 export { router };
