038060a9a0a40f1982cdc076df926b0cb21ff7f9
CookieDasora <valterliviaamaral@gmail.com>
2022-01-17 09:58:36 -0300

    Implemented file upload system

--- src/controllers/uploadFile.controller.ts
+++ src/controllers/uploadFile.controller.ts
@@ -3,8 +3,6 @@ import uploadFileService from '../services/uploadFile.service';
 
 class uploadFileController {
   async handle(req: Request, res: Response) {
-    console.log(req.file);
-
     const service = new uploadFileService();
     const result = await service.execute();
 
